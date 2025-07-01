7867c3588cb62d3794a8c874cc063e897090857c
李太白 <36808766+ghhccghk@users.noreply.github.com>
2025-07-01 22:47:48 +0800

    Update android.sh

--- android.sh
+++ android.sh
@@ -42,7 +42,7 @@ echo "=====[Building Node.js]====="
 
 cp $WORKSPACE/android-configure ./
 ./android-configure ~/android-ndk-r21b $2 24 $WITH_SSL
-make LDFLAGS="-Wl,-z,common-page-size=16384" -j8
+make LDFLAGS="-stdlib=libc++ -Wl,-z,common-page-size=16384" CXXFLAGS="-stdlib=libc++ -include cstdint" -j8
 
 mkdir -p ../puerts-node/nodejs/lib/Android/$OUTPUT/
 
